7bf5e5c5390fae96eea82dab963d3cedceb0590f
Mr. Charkuils <charkuils@proton.me>
2023-02-13 16:13:08 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,6 +76,17 @@ require'nvim-tree'.setup{
     }
 }
 
+local function open_nvim_tree(data)
+  local directory = vim.fn.isdirectory(data.file) == 1
+  if not directory then
+    return
+  end
+  vim.cmd.cd(data.file)
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 -- Lualine
 require'lualine'.setup{
     options = {
