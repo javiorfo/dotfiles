b8414c34c630f4b476b20bdf55815fbaea1366f0
Javier Orfo <javierorfo@protonmail.com>
2023-05-22 10:52:58 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,7 +63,7 @@ require("lazy").setup({
     {
         "javiorfo/nvim-fuel",
         lazy = true,
-        ft = { "c", "java", "haskell", "lua", "rust", "zig" },
+        ft = { "c", "java", "lua", "rust" },
         dependencies = { "javiorfo/nvim-popcorn" },
         config = function()
             require'fuel'.setup { popup = true }
@@ -174,7 +174,7 @@ require("lazy").setup({
     {
         "neovim/nvim-lspconfig",
         lazy = true,
-        ft = { "c", "haskell", "lua", "rust", "zig" },
+        ft = { "c", "lua", "rust" },
         config = function()
             lsp_icons()
           
@@ -192,9 +192,6 @@ require("lazy").setup({
 
             -- Rust
             lsp_config.rust_analyzer.setup{ on_attach = on_attach }
-        
-            -- Zig
-            lsp_config.zls.setup{ on_attach = on_attach }
 
             -- Lua
             lsp_config.lua_ls.setup {
