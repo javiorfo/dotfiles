534a0ce6f73e95fb78fcf750f0f06b81951fc527
Javier Orfo <javierorfo@protonmail.com>
2023-06-12 23:47:38 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -170,7 +170,7 @@ require("lazy").setup({
     {
         "neovim/nvim-lspconfig",
         lazy = true,
-        ft = { "c", "lua", "go", "rust" },
+        ft = { "lua", "go", "rust" },
         config = function()
             lsp_icons()
           
@@ -179,9 +179,6 @@ require("lazy").setup({
             end
 
             local lsp_config = require'lspconfig'
-        
-            -- C
-            lsp_config.clangd.setup{ on_attach = on_attach }
             
             -- Go
             lsp_config.gopls.setup{ on_attach = on_attach }
