4dac51674e0fbc2d910b769b160d8a14b2539d3b
Javier Orfo <javierorfo@protonmail.com>
2023-07-13 11:50:05 -0300

    Update archinstall.sh

--- .config/xmonad/archinstall.sh
+++ .config/xmonad/archinstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Author: Progrimorio
+# Author: Javier Orfo
 
 sudo pacman -S --needed xmonad xmonad-contrib xmobar pamixer xautolock pulseaudio-alsa feh alacritty ttf-ubuntu-mono-nerd
 
